68e57b209648a223b2a70ee97ca079136623dbe1
dulkarsaman <dulkarsaman7@gmail.com>
2025-08-05 20:20:08 +0530

    Fix:CORS configuration - add missing headers

--- backend/syntax/syntax/settings.py
+++ backend/syntax/syntax/settings.py
@@ -190,10 +190,30 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL='accounts.User'
 
 CORS_ALLOWED_ORIGINS=[
-     "http://localhost:5173",
      "https://syntax.dulkarsaman.site",
 ]
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 
